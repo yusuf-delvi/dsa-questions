0118560f549f335197fa398df89df8de573593c5
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-11-27 22:49:28 +0530

    problem frequency counter

---
+++ problem-frequency-counter.js
@@ -0,0 +1,9 @@
+/*Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.*/
+
+function areThereDuplicates(...params) {
+	return params.length !== new Set(params).size;
+}
+
+areThereDuplicates(1, 2, 3); // false
+areThereDuplicates(1, 2, 2); // true
+areThereDuplicates('a', 'b', 'c', 'a'); // true
