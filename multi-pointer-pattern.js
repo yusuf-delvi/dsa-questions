a71edc1ed7cbe2d41427f8fa8753b2c1c5677e6b
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-12-01 18:37:31 +0530

    average pair - multi pointer

--- multi-pointer-pattern.js
+++ multi-pointer-pattern.js
@@ -23,3 +23,31 @@ countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
 countUniqueValues([]); // 0
 countUniqueValues([-2, -1, -1, 0, 1]); // 4
+
+/* AveragePair
+Write a function called averagePair. Given a sorted array of integers and a target average, 
+determine if there is a pair of values in the array where the average of the pair equals the target average. 
+There may be more than one pair that matches the average target.*/
+function averagePair(arr, avg) {
+	let first = 0;
+	let last = arr.length - 1;
+
+	while (first < last) {
+		let sumAvg = (arr[first] + arr[last]) / 2;
+
+		if (sumAvg === avg) return true;
+
+		if (sumAvg < avg) {
+			first++;
+		} else {
+			last--;
+		}
+	}
+
+	return false;
+}
+
+console.log(averagePair([1, 2, 3], 2.5)); // true
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
+console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
+console.log(averagePair([], 4)); // false
