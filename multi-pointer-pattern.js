1c290fc85e3fc51cfc3959edaa4ff2ee8522432e
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-12-01 18:53:34 +0530

    issubsequence multi pointer

--- multi-pointer-pattern.js
+++ multi-pointer-pattern.js
@@ -51,3 +51,26 @@ console.log(averagePair([1, 2, 3], 2.5)); // true
 console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)); // true
 console.log(averagePair([-1, 0, 3, 4, 5, 6], 4.1)); // false
 console.log(averagePair([], 4)); // false
+
+/*Write a function called isSubsequence which takes in two strings and checks whether the characters in the first string form 
+a subsequence of the characters in the second string. In other words, the function should check whether 
+the characters in the first string appear somewhere in the second string, without their order changing.*/
+function isSubsequence(str1, str2) {
+	var i = 0;
+	var j = 0;
+
+	if (!str1) return true;
+
+	while (j < str2.length) {
+		if (str1[i] === str2[j]) i++;
+		if (i === str1.length) return true;
+		j++;
+	}
+
+	return false;
+}
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
